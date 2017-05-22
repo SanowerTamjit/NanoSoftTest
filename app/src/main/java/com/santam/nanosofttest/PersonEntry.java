3def3769551efb3410f170e844d596c030fc74fb
Tamjit <sanowertamjit@gmail.com>
2017-05-22 14:53:40 +0600

    Testing Project

--- app/src/main/java/com/santam/nanosofttest/PersonEntry.java
+++ app/src/main/java/com/santam/nanosofttest/PersonEntry.java
@@ -1,28 +1,28 @@
-package com.santam.nanosofttest;
-
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-
-import java.util.UUID;
-
-public class PersonEntry extends AppCompatActivity {
-    String uniqueID = UUID.randomUUID().toString();
-    EditText mName, mAge, mDob, mLatiude, mLongitude;
-    Button mSubmit, mClose;
-    private LinearLayout linearLayout;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_person_entry);
-        mName = (EditText) findViewById(R.id.e);
-        linearLayout = (LinearLayout) findViewById(R.id
-                .linearLayout);
-
-
-
-    }
-}
+//package com.santam.nanosofttest;
+//
+//import android.os.Bundle;
+//import android.support.v7.app.AppCompatActivity;
+//import android.widget.Button;
+//import android.widget.EditText;
+//import android.widget.LinearLayout;
+//
+//import java.util.UUID;
+//
+//public class PersonEntry extends AppCompatActivity {
+//    String uniqueID = UUID.randomUUID().toString();
+//    EditText mName, mAge, mDob, mLatiude, mLongitude;
+//    Button mSubmit, mClose;
+//    private LinearLayout linearLayout;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_person_entry);
+//        mName = (EditText) findViewById(R.id.tex)
+//        linearLayout = (LinearLayout) findViewById(R.id
+//                .linearLayout);
+//
+//
+//
+//    }
+//}
